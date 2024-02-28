ea17e3769b2660e4305d10035278584b2ae053f7
Maks <mishkin_mi@edu.surgu.ru>
2024-02-28 21:26:00 +0500

    finnaly

--- Multithreading(Lab3)/Multithreading(Lab3).cpp
+++ Multithreading(Lab3)/Multithreading(Lab3).cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 const int ARRAY_SIZE = 10000;
 
-void fillArray(vector<int> arr, int size) {
+void fillArray(vector<int> &arr, int size) {
     srand(time(nullptr));
     for (int i = 0; i < size; ++i) {
         arr.push_back(rand() % 1000);
@@ -32,13 +32,15 @@ struct quickSortData
 
 
 DWORD WINAPI bubbleSort(LPVOID data) {
-    for (int i = 0; i < data.size - 1; ++i) {
-        for (int j = 0; j < data.size - i - 1; ++j) {
-            if (data.arr[j] > data.arr[j + 1]) {
-                swap(data.arr[j], data.arr[j + 1]);
+    bubbleSortData sortData = *(bubbleSortData*)data;
+    for (int i = 0; i < sortData.size - 1; ++i) {
+        for (int j = 0; j < sortData.size - i - 1; ++j) {
+            if (sortData.arr[j] > sortData.arr[j + 1]) {
+                swap(sortData.arr[j], sortData.arr[j + 1]);
             }
         }
     }
+    return 0;
 }
 
 int partition(vector<int> arr, int low, int high) {
@@ -57,24 +59,38 @@ int partition(vector<int> arr, int low, int high) {
 }
 
 DWORD WINAPI quickSort(LPVOID data) {
-    if (data.low < data.high) {
-        int pivotIndex = partition(data.arr, data.low, data.high);
+    quickSortData sortData = *(quickSortData*)data;
+    if (sortData.low < sortData.high) {
+        int pivotIndex = partition(sortData.arr, sortData.low, sortData.high);
 
         quickSortData newData;
-
-        newData.low = data.low;
+        newData.arr = sortData.arr;
+        newData.low = sortData.low;
         newData.high = pivotIndex - 1;
-        quickSort(newData);
+        quickSort(&newData);
 
         newData.low = pivotIndex + 1;
-        newData.high = data.high;
-        quickSort(newData);
+        newData.high = sortData.high;
+        quickSort(&newData);
     }
+    return 0;
 }
 
-DWORD WINAPI func(LPVOID x)
+void printThreadTimes(HANDLE threadBubbleSortHandle)
 {
-    return 1;
+    FILETIME fileTime1, fileTime2, fileTime3, fileTime4;
+    GetThreadTimes(threadBubbleSortHandle, &fileTime1, &fileTime2, &fileTime3, &fileTime4);
+
+    SYSTEMTIME sysTime1, sysTime2, sysTime3, sysTime4;
+    FileTimeToSystemTime(&fileTime1, &sysTime1);
+    FileTimeToSystemTime(&fileTime2, &sysTime2);
+    FileTimeToSystemTime(&fileTime3, &sysTime3);
+    FileTimeToSystemTime(&fileTime4, &sysTime4);
+
+    cout << "CreationTime = " << sysTime1.wHour << "hour " << sysTime1.wMinute << "min " << sysTime1.wSecond << "sec " << sysTime1.wMilliseconds << "ms" << endl;
+    cout << "ExitTime = " << sysTime2.wHour << "hour " << sysTime2.wMinute << "min " << sysTime2.wSecond << "sec " << sysTime2.wMilliseconds << "ms" << endl;
+    cout << "KernelTime = " << sysTime3.wMilliseconds << " ms" << endl;
+    cout << "UserTime = " << sysTime4.wMilliseconds << " ms" << endl << endl;
 }
 
 
@@ -95,21 +111,24 @@ int main()
     quickData.arr = quickSortArray;
     quickData.low = 0;
     quickData.high = ARRAY_SIZE - 1;
+
+
+    cout << "Bubble sort:" << endl;
+    HANDLE threadBubbleSortHandle = CreateThread(NULL, CREATE_SUSPENDED, &bubbleSort, &bubbleData, 0, NULL);
+    ResumeThread(threadBubbleSortHandle);
+    SetThreadPriority(threadBubbleSortHandle, THREAD_PRIORITY_LOWEST);
+    WaitForSingleObject(threadBubbleSortHandle, INFINITE);
+    TerminateThread(threadBubbleSortHandle, 0);
+    printThreadTimes(threadBubbleSortHandle);
     
-    HANDLE threadBubbleSort = CreateThread(NULL, 0, &func, &bubbleData, 0, NULL);
+    cout << "Quick sort:" << endl;
+    HANDLE threadQuickSortHandle = CreateThread(NULL, CREATE_SUSPENDED, &quickSort, &quickData, 0, NULL);
+    ResumeThread(threadQuickSortHandle);
+    SetThreadPriority(threadQuickSortHandle, THREAD_PRIORITY_HIGHEST);
+    WaitForSingleObject(threadQuickSortHandle, INFINITE);
+    TerminateThread(threadQuickSortHandle, 0);
+    printThreadTimes(threadQuickSortHandle);
 
-    HANDLE threadBubbleSort = CreateThread(NULL, 0, &bubbleSort, &bubbleData, 0, NULL);
-    HANDLE threadQuickSort = CreateThread(NULL, 0,  &quickSort, &quickData, 0, NULL);
-
-    WaitForSingleObject(threadBubbleSort, INFINITE);
-    WaitForSingleObject(threadQuickSort, INFINITE);
-
-
-    //bubbleSortThread.get_id();
-    //bubbleSortThread.get_id();
-
-    //bubbleSortThread.join();
-    //quickSortThread.join();
-
-    //bubbleSortThread.
+    CloseHandle(threadBubbleSortHandle);
+    CloseHandle(threadQuickSortHandle);
 }
